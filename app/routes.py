9d1cc2d5e1517e923abbb96dc29c48b9ed9c5b61
Cody Johnson <Cody.Johnson@mottmac.com>
2023-07-30 21:59:06 -0500

    user logic

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import flash, redirect, render_template, url_for
-from flask_login import current_user, login_user
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
@@ -8,8 +9,9 @@ from app.models import User
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index(username=None):
-    return render_template("index.html", username=username)
+    return render_template("index.html", title="Home Page")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -21,12 +23,26 @@ def login():
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+
         if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
+
+        # direct back to accessing login protected pages after loggin in
+        # return to "index" if not.
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    login_user()
+    return redirect(url_for("index"))
 
 
 @app.route("/map")
