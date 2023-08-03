d58b76c12612c7189ef850ab126c24312df5056e
Cody Johnson <Cody.Johnson@mottmac.com>
2023-08-02 22:04:40 -0500

    user registration

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from flask import flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_required, login_user
+from flask_login import current_user, login_required, login_user, logout_user
 from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -41,8 +41,25 @@ def login():
 
 @app.route("/logout")
 def logout():
-    login_user()
+    logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(
+            f"Welcome {form.username.data}. Congrats on registering with the DBH logs!"
+        )
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Registration", form=form)
 
 
 @app.route("/map")
